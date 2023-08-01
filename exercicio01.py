4440174e151ab45776fb1ee8ef917ba422990de1
Christopher Alves <christo03082007@gmail.com>
2023-08-01 09:59:48 -0300

    Adicionado exercicio01.py e exercicio02.py

---
+++ exercicio01.py
@@ -0,0 +1,16 @@
+'''
+Elabore um progama que pergunte ao aluno suas 3 notas escolares.
+O progama deverá calcular a média das 3 notas inseridas e
+exibir esta média.
+'''
+
+nota1 = float(input("Informe suas 3 notas. 1º: "))
+nota2 = float(input("2º: "))
+nota3 = float(input("3º: "))
+media = (nota1 + nota2 + nota3) / 3
+
+print("Sua média aritmética é", media)
+
+
+
+
---
+++ exercicio02.py
@@ -0,0 +1,15 @@
+'''
+Fazer um progama que pergunte um número inteiro e apresente ao usuário
+o antecessor e o sucessor do número informado.
+'''
+
+num = int(input("Fale-me um número inteiro "))
+
+ant = num - 1
+suc = num + 1
+
+print("O antecessor é", ant, "e o sucessor é", suc)
+
+
+
+
